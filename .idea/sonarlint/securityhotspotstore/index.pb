28dba07a4c19e822ed4501bb38869f024feb4018
Furkan Ayaz <furkanayaz.dev@gmail.com>
2023-11-06 11:12:04 +0300

    Cellular v1.1 released.

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -13,3 +13,7 @@ D
 app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+C
+settings.gradle.kts,7\4\749edfcc96398253e5b3416184e95c46621da850
+;
+jitpack.yml,3\f\3f384c5ac91d01ea9ab11981ab87f1d958f63284
--- cellular/build.gradle.kts
+++ cellular/build.gradle.kts
@@ -48,7 +48,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.furkanayaz"
             artifactId = "cellular"
-            version = "1.0"
+            version = "1.1"
 
             afterEvaluate {
                 from(components["release"])
